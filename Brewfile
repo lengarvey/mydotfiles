feff7bb867d11758d5aae9b6333951e64d457810
Leonard Garvey <lengarvey@gmail.com>
2014-10-21 22:57:27 +1100

    Add support for bootstrapping a new setup

---
+++ Brewfile
@@ -0,0 +1,27 @@
+install caskroom/cask/brew-cask
+install chruby
+install direnv
+install git
+install hub
+install imagemagick
+install nginx
+install node
+install openssl
+install postgresql
+install reattach-to-user-namespace
+install redis
+install ruby-install
+install tmux
+install vim
+install wget
+
+cask install airfoil
+cask install alfred
+cask install dropbox
+cask install flowdock
+cask install google-chrome
+cask install rdio
+cask install skype
+cask install steam
+cask install teamspeak-client
+cask install transmission
---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# change to zsh
+chsh -s /bin/zsh
+
+# install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# symlink dotfiles
+ln -s ~/.dotfiles/direnvrc ~/.direnvrc
+ln -s ~/.dotfiles/gitignore ~/.gitignore
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/vimrc ~/.vim
+ln -s ~/.dotfiles/vimrc/.vimrc ~/.vimrc
+
+# install brewfile
+brew tap homebrew/boneyard
+
+echo "Run brew bundle!"
